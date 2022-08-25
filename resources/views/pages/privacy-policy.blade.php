2a60451f2a4c58e82ae347e907719b58b93d62a3
mafifanars <sm.afifanaly@gmail.com>
2022-08-25 12:20:53 +0700

    menambah halaman ketentuan layanan dan kebijakan privasi

--- resources/views/pages/privacy-policy.blade.php
+++ resources/views/pages/privacy-policy.blade.php
@@ -5,22 +5,29 @@
 @section('pages')
 
 {{-- Title --}}
-<section id="subpage-title" class="" style="padding-bottom: -50px;">
+<section id="subpage-title" class="section-bg" style="padding-bottom: -50px;">
 
-    <div class="container">
+    {{-- <div class="container">
+        <header class="section-header">
+            <h3>Tanya dan Jawab</h3>
+            <p>Tanya dan Jawab untuk pekerja (mitra)</p>
+        </header>
+    </div> --}}
+
+</section>
+  
+{{-- FAQ --}}
+<section id="faq">
+    
+    <div class="container mt-5">
         <header class="section-header">
             <h3>Kebijakan Privasi</h3>
             {{-- <p>Tanya dan Jawab untuk pekerja (mitra)</p> --}}
         </header>
     </div>
 
-</section>
-  
-{{-- FAQ --}}
-<section id="faq" style="margin-top: -50px;">
     <div class="container">
         <div class="row portfolio-container col-md-6 col-lg-12 mb-5">
-            asdasd
         </div>
     </div>
 </section>
--- resources/views/pages/term-of-services.blade.php
+++ resources/views/pages/term-of-services.blade.php
@@ -1,26 +1,33 @@
 @extends('layouts.main')
 
-@section('title', 'JasaKita | Kebijakan Privasi')
+@section('title', 'JasaKita | Ketentuan Layanan')
 
 @section('pages')
 
 {{-- Title --}}
-<section id="subpage-title" class="" style="padding-bottom: -50px;">
+<section id="subpage-title" class="section-bg" style="padding-bottom: -50px;">
 
-    <div class="container">
+    {{-- <div class="container">
         <header class="section-header">
-            <h3>Kebijakan Privasi</h3>
-            {{-- <p>Tanya dan Jawab untuk pekerja (mitra)</p> --}}
+            <h3>Tanya dan Jawab</h3>
+            <p>Tanya dan Jawab untuk pekerja (mitra)</p>
         </header>
-    </div>
+    </div> --}}
 
 </section>
   
 {{-- FAQ --}}
-<section id="faq" style="margin-top: -50px;">
+<section id="faq">
+    
+    <div class="container mt-5">
+        <header class="section-header">
+            <h3>Ketentuan Layanan</h3>
+            {{-- <p>Tanya dan Jawab untuk pekerja (mitra)</p> --}}
+        </header>
+    </div>
+
     <div class="container">
         <div class="row portfolio-container col-md-6 col-lg-12 mb-5">
-            asdasd
         </div>
     </div>
 </section>
