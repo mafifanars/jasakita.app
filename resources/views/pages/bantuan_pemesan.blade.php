cdff3d1044177000e8ebb46b7ec52839322634c7
mafifanars <sm.afifanaly@gmail.com>
2022-08-25 12:00:31 +0700

    merapikan qna

--- resources/views/pages/bantuan_pemesan.blade.php
+++ resources/views/pages/bantuan_pemesan.blade.php
@@ -7,6 +7,18 @@
 {{-- Title --}}
 <section id="subpage-title" class="section-bg" style="padding-bottom: -50px;">
 
+    {{-- <div class="container">
+        <header class="section-header">
+            <h3>Tanya dan Jawab</h3>
+            <p>Tanya dan Jawab untuk pemesan</p>
+        </header>
+    </div> --}}
+    
+</section>
+
+{{-- FAQ --}}
+<section id="faq">
+
     <div class="container">
         <header class="section-header">
             <h3>Tanya dan Jawab</h3>
@@ -14,10 +26,6 @@
         </header>
     </div>
 
-</section>
-  
-{{-- FAQ --}}
-<section id="faq" style="margin-top: -50px;">
     <div class="container">
         <div class="row portfolio-container col-md-6 col-lg-12 mb-5">
             <ul id="faq-list" class="wow fadeInUp col-lg-12 portfolio-item filter-app">
